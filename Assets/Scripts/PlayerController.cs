8d2083efa433acc96ed3ecb61ce2992dad939a84
dschaad <97546064+dschaad@users.noreply.github.com>
2023-01-25 18:57:01 -0700

    First commit, existing project and .gitignore

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Overview
+
+{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+{Provide a description the game that you wrote. Describe how to play your game.}
+
+{Describe your purpose for writing this software.}
+
+# Development Environment
+
+I used Unity version 
+
+{Describe the programming language that you used and any libraries.}
+
+# Useful Websites
+
+* [Unity Learn](https://learn.unity.com/)
+* [Stack Overflow](https://stackoverflow.com/)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* Item 1
+* Item 2
+* Item 3
